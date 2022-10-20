ce094e96900b09176fdb4a1fcd4a42682a9b5dee
qundus <72963129+qundus@users.noreply.github.com>
2022-10-20 14:13:32 +0300

    update: make shortcuts

--- _/sweetjsx/helpers/uno.helper.ts
+++ _/sweetjsx/helpers/uno.helper.ts
@@ -9,7 +9,7 @@
 import { Theme } from "@unocss/preset-mini";
 import fs from "node:fs";
 import path from "node:path";
-import unocss, { UserConfig, mergeDeep, DeepPartial } from "unocss";
+import unocss, { UserConfig, mergeDeep, DeepPartial, UserShortcuts } from "unocss";
 import { transformerDirectives } from "unocss";
 import type { DeepVoid, FilterKeysOf, Pick } from ".";
 import { deepMerge } from ".";
@@ -626,60 +626,96 @@ export function makeThemeRules(
 }
 
 /**
- * Create unocss shortcuts and safelists.
- * @param id will be added before every style function name
- * @param replacers put between utility and tag like `light` to identify light theme for example
- * @param styles build styles according and let unocss handle the rest,
- * start by naming the function you want then add basic windicss class atributes.
- * examples (with light theme only "li"):
- * - light theme text color and background:
- *  >["app-", "bg$bg_text$t"] -> ` bg-li-bg text-li-t `
- * - text light theme with hover:
- *  >["select-", "text$t", "hover"] -> ` text-li-t hover:text-li-t$h ` <-
- * - tell it that you defined `hov` as the short for hover in your windi.config:
- *  >["code-", "text$t", "hover:hov"] -> ` text-li-t hover:text-li-t$hov `
- * - define multiple actions|variants:
- *  >["app-", "border$b", "hover:h_active:ac"] -> ` border-li-t hover:border-li-t$h active:border-li-b$ac `
- * - when tags are under certain groups
- *  >["app-", "bg$bg", undefined, "btn"] -> ` bg-li-btn-bg `
- * - specify other tags for actions
- *  >["app-", "bg$bg", "hover:text$t,border$bg", "btn"] ->
- * ` bg-li-btn-bg hover:(text-li-btn-t$h border-li-btn-bg)`
- * @returns
+ * Create unocss opinionated shortcuts.
  */
-//  export function makeShortcuts(
-//   id: string,
-//   replacers: string[],
-//   styles: [
-//     key: string,
-//     pattern: string,
-//     actions?: string | undefined,
-//     group?: string | undefined
-//   ][],
-//   splits?: Splitters,
-//   filler?: string
-// ): { safelist: string[]; shortcuts: { [key: string]: string } } {
-//   const result = {};
-//   splits = { ...configDefaults.splits, ...splits };
-//   if (!filler) filler = configDefaults.filler;
-//   styles.forEach((s) => {
-//     const key = s[0];
-//     s.shift();
-//     // adding spaces before and after to avoid clashing with other user defined styles
-//     const style = ` ${makeWhole(s.join(splits.func), filler, splits)} `;
-//     // making the shortcut
-//     if (replacers) {
-//       replacers.forEach((sep) => {
-//         result[`${id}${s[0]}${sep}`] = style
-//           .split(configDefaults.filler)
-//           .join(sep);
-//       });
-//     } else {
-//       result[`${id}${s[0]}`] = style.split(configDefaults.filler).join("");
-//     }
-//   });
-//   return {
-//     safelist: Object.keys(result),
-//     shortcuts: result,
-//   };
-// }
+export function makeShortcuts(_unocssConfig: Config["unocssConfig"]) {
+  let result: UserShortcuts = [];
+  // flex
+  result.push({
+    flex$: "flex flex-nowrap relative",
+    flex$wrap: "flex$ flex-wrap",
+    flex$rows: "flex$ flex-col",
+    flex$rows$wrap: "flex$rows flex$wrap",
+    flex$cols: "flex$ flex-row",
+    flex$cols$wrap: "flex$cols flex$wrap",
+  });
+
+  // area
+  result.push(
+    {
+      area$: "flex-grow-[1] flex-shrink-[1] basis-[99%]",
+      area$max: "flex-grow-[1] flex-shrink-[1] basis-[max]",
+      area$sm: "flex-grow-[0.3] flex-shrink-[0.3] basis-[10%]",
+      area$sm$1: "flex-grow-[0.6] flex-shrink-[0.4] basis-[20%]",
+      area$sm$2: "flex-grow-[0.8] flex-shrink-[0.5] basis-[35%]",
+      area$sm$3: "flex-grow-[0.9] flex-shrink-[0.6] basis-[50%]",
+      area$lg: "flex-grow-[1.3] flex-shrink-[0.7] basis-[60%]",
+      area$lg$1: "flex-grow-[1.6] flex-shrink-[0.9] basis-[70%]",
+      area$lg$2: "flex-grow-[2] flex-shrink-[1] basis-[80%]",
+    }
+  );
+
+  // size
+  result.push(
+    {
+      fit$w: "!w-max !max-w-max",
+      fit$w$m: "fit$w !min-w-max",
+      fit$h: "!h-max !max-h-max",
+      fit$h$m: "fit$h !min-h-max",
+      fit$m: "fit$w$m fit$h$m",
+      fit$: "fit$w fit$h",
+      lock$w: "!min-w-full !w-full !max-w-full",
+      lock$ws: "!min-w-screen !w-screen !max-w-screen",
+      lock$h: "!min-h-full !h-full !max-h-full",
+      lock$hs: "!min-h-screen !h-screen !max-h-screen",
+      lock$: "lock$w lock$h",
+      lock$s: "lock$ws lock$hs",
+
+      extend$w: "!w-full !max-w-full",
+      extend$w$m: "!w-full !min-w-full",
+      extend$ws: "!w-screen !max-w-screen",
+      extend$ws$m: "extend$ws !min-w-screen",
+      extend$h: "!h-full !max-h-full",
+      extend$h$m: "extend$h !min-h-full",
+      extend$h$s: "!h-screen !max-h-screen",
+      extend$h$s$m: "extend$h$s !min-h-screen",
+      extend$s$m: "extend$w$s$m extend$h$s$m",
+      extend$s: "extend$w$s extend$h$s",
+      extend$m: "extend$w$m extend$h$m",
+      extend$: "extend$w extend$h",
+      trim$: "!overflow-hidden",
+      trim$w: "!overflow-x-hidden !overflow-y-visible",
+      trim$h: "!overflow-y-hidden !overflow-x-visible",
+    }
+  );
+
+  // position
+  result.push({
+    child$between: "justify-between content-between place-content-between",
+    child$around: "justify-around content-around place-content-around",
+    pull$self: "self-start place-self-start",
+    pull$text: "text-left rtl:text-tight",
+    pull$items:
+      "items-start justify-start content-start place-content-start object-left rtl:object-right",
+    pull$: "pull$self pull$text pull$items",
+    push$self: "self-end place-self-end",
+    push$text: "text-right rtl:text-left",
+    push$items:
+      "items-end justify-end content-end place-content-end object-right rtl:object-left",
+    push$: "push$self push$text push$items",
+    fight$self: "self-center place-self-center",
+    fight$text: "text-center",
+    fight$items:
+      "items-center justify-center content-center object-center place-items-center",
+    fight$: "fight$self fight$text fight$items",
+  });
+
+  // append original user config shortcuts 
+  if (Array.isArray(_unocssConfig.shortcuts)) {
+    result.push(..._unocssConfig.shortcuts);
+  } else {
+    result.push(_unocssConfig.shortcuts);
+  }
+  _unocssConfig.shortcuts = result;
+  return _unocssConfig;
+}
